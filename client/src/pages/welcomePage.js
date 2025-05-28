c421815055f8511598c36a7ad5f4deb7936d5de0
starshynova <l.starshynova@gmail.com>
2025-05-28 13:18:58 +0200

    added instruction button

--- client/src/pages/welcomePage.js
+++ client/src/pages/welcomePage.js
@@ -10,6 +10,7 @@ export const welcomePage = () => {
   container.classList.add('container');
   const containerHeader = document.createElement('div');
   containerHeader.classList.add('container-header');
+  containerHeader.style.fontSize = "48px";
   containerHeader.textContent = 'Welcome to WoordenQuiz!';
   container.appendChild(containerHeader);
   document.getElementById('user-interface').appendChild(container);
@@ -20,39 +21,52 @@ export const welcomePage = () => {
   const descriptionContainer = document.createElement('div');
   descriptionContainer.classList.add('description-container');
   container.appendChild(descriptionContainer);
+  
+  const description = document.createElement('h2');
+  description.textContent = "This app uses a unique repetition system that helps you learn Dutch words efficiently and for a long time. Each word will be shown to you again and again - until you memorise it! Thanks to this repetition logic, you won't lose important knowledge and will quickly expand your vocabulary."
+  descriptionContainer.appendChild(description);
 
-  const stagesDescriptions = [
-    'Initial introduction to new words. The card shows a Dutch word. Tap to reveal the English translation.',
-    'Visual enhancement. The card shows again a Dutch word — tap to see the English translation.',
-    'Develop active memory. The card shows an English word — tap to reveal the Dutch translation.',
-    'Check understanding with minimal difficulty. The card shows a Dutch word. Choose the correct English translation from two options.',
-    'Repeat: again a Dutch word and two answer choices.',
-    'More difficult task of recognising. The card shows a Dutch word with four English translation options.',
-    'Repeat: again a Dutch word with four answer options.',
-    'Final check in the reverse direction. The card shows an English word — choose the correct Dutch translation from four options.',
-  ];
+  // const stagesDescriptions = [
+  //   'Initial introduction to new words. The card shows a Dutch word. Tap to reveal the English translation.',
+  //   'Visual enhancement. The card shows again a Dutch word — tap to see the English translation.',
+  //   'Develop active memory. The card shows an English word — tap to reveal the Dutch translation.',
+  //   'Check understanding with minimal difficulty. The card shows a Dutch word. Choose the correct English translation from two options.',
+  //   'Repeat: again a Dutch word and two answer choices.',
+  //   'More difficult task of recognising. The card shows a Dutch word with four English translation options.',
+  //   'Repeat: again a Dutch word with four answer options.',
+  //   'Final check in the reverse direction. The card shows an English word — choose the correct Dutch translation from four options.',
+  // ];
 
-  stagesDescriptions.forEach((description, index) => {
-    const descriptionElement = document.createElement('div');
-    descriptionElement.classList.add('description');
-    const descriptionHeading = document.createElement('h3');
-    descriptionHeading.textContent = `Stage ${index + 1}`;
-    const descriptionText = document.createElement('h4');
-    descriptionText.textContent = description;
+  // stagesDescriptions.forEach((description, index) => {
+  //   const descriptionElement = document.createElement('div');
+  //   descriptionElement.classList.add('description');
+  //   const descriptionHeading = document.createElement('h3');
+  //   descriptionHeading.textContent = `Stage ${index + 1}`;
+  //   const descriptionText = document.createElement('h4');
+  //   descriptionText.textContent = description;
 
-    descriptionElement.appendChild(descriptionHeading);
-    descriptionElement.appendChild(descriptionText);
-    descriptionContainer.appendChild(descriptionElement);
-  });
+  //   descriptionElement.appendChild(descriptionHeading);
+  //   descriptionElement.appendChild(descriptionText);
+  //   descriptionContainer.appendChild(descriptionElement);
+  // });
 
-  const containerStartButton = document.createElement('div');
-  containerStartButton.classList.add('container-next-button');
-  container.appendChild(containerStartButton);
+  const containerButtons = document.createElement('div');
+  containerButtons.classList.add('container-next-button');
+  container.appendChild(containerButtons);
+
+  const instructionButton = document.createElement('button');
+  instructionButton.classList.add('next-button');
+  instructionButton.style.marginRight = "20px";
+  instructionButton.textContent = 'Instructions';
+  containerButtons.appendChild(instructionButton);
 
   const startButton = document.createElement('button');
   startButton.classList.add('next-button');
+  startButton.style.marginLeft = "20px";
   startButton.textContent = 'Start WoordenQuiz!';
-  containerStartButton.appendChild(startButton);
+  containerButtons.appendChild(startButton);
+
+  
 
   const token = localStorage.getItem('token');
   if (token) {
